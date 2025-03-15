6562554bdeecb4dd58af31f741ee5cd2c9a6e239
vignesh3586 <vigneshr3586@gmail.com>
2025-03-15 12:06:22 +0530

    update

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Install PHP (Change version if needed)
-sudo apt update && sudo apt install -y php-cli php-mbstring php-xml unzip curl
-
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+
 
 # Install Laravel dependencies
 composer install --no-dev --optimize-autoloader
